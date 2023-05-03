0c63dcc65d3318240874a45a672df80e0bae42b0
Jinkyung Lee <83896975+ijinkyung@users.noreply.github.com>
2023-05-03 19:41:29 +0900

    Add : footer 구현 완료

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer footer-center p-16 bg-white  text-primary-content h-1/4">
+      <div>
+        <img
+          alt="logo"
+          src="images/meerkat.png"
+          className="inline-block fill-current w-16 h-16 text-black"
+        />
+        <p className="font-bold text-base text-black">
+          mgkkm Industries Ltd. <br />
+          Providing reliable tech since 2023
+        </p>
+        <p className="text-black mt-5">Copyright © 2023 - All right reserved</p>
+      </div>
+    </footer>
+  );
 }
