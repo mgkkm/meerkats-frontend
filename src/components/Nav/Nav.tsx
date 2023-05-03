0c9e25c17158c7eb2da9545374cda506456b683b
Jinkyung Lee <83896975+ijinkyung@users.noreply.github.com>
2023-05-03 19:00:07 +0900

    Modify : scroll 감지 방향 수정 완료

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import SideBar from './components/SideBar';
 
-export default function Nav() {
-  return <div>Nav</div>;
+interface HidePropsType {
+  show: boolean;
+}
+
+export default function Nav({ show }: HidePropsType) {
+  return (
+    <div
+      className={`navbar bg-base-100 py-6 px-7 fixed top-0 z-10 visible transition duration-500 ease-in-out shadow  ${
+        show ? '' : 'opacity-0'
+      }`}
+    >
+      <div className="navbar-start">
+        <div className="dropdown">
+          <SideBar />
+        </div>
+      </div>
+      <div className="navbar-center">
+        <Link to="/">
+          <img alt="logo" src="/images/logo_b.png" className="w-44" />
+        </Link>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-ghost btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-7 w-7"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
 }
