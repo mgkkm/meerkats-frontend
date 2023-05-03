0274e909b87fdaac4cea7b1c1cce2914cf93333d
hyun joo oh <58460131+hyunjoo1130@users.noreply.github.com>
2023-05-03 22:15:15 +0900

    [Feat] 카카오 소셜 로그인 구현 완료 (#2)
    feature/kakaoLogin
    
    - Feat : 카카오 소셜 로그인 구현 완료
    
    - Modify: 코드 수정 리뷰 사항 반영
    - span 태그를 p 태그로 변경하여, 띄어쓰기를 표기가 아닌 margin값으로 변경
    - useAxios 적용
    - jwt-decode 라이브러리 활용하여, 토큰 내 userId 값, userNickname recoil로 상태관리

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import UserForm from './components/UserForm';
+import KakaoLoginBtn from './components/KakaoLoginBtn';
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="container xl mt-24 pt-28 pb-9 bg-mkBg text-mkBlack">
+      <div className="w-44 m-auto pt-4 pb-16 text-center">
+        <img src="/images/logo_b.png" alt="logo" className="w-full" />
+      </div>
+      <UserForm />
+      <div className="mt-12 text-center">
+        <span className="mr-8">아이디 찾기</span>|
+        <span className="mx-8">비밀번호 찾기</span>|
+        <Link to="/signin">
+          <span className="ml-8">회원가입</span>
+        </Link>
+      </div>
+      <div className="flex justify-center items-center mt-20 text-center text-mkBlack opacity-50">
+        <hr className="inline-block border-mkGray w-1/4 mr-10" />
+        <span>다른 서비스 계정으로 로그인</span>
+        <hr className="inline-block border-mkGray w-1/4 ml-10" />
+      </div>
+      <div className="socialLoginBtn mt-5 text-center">
+        <div className="inline-block m-auto mt-6 mx-3">
+          <KakaoLoginBtn />
+        </div>
+      </div>
+    </div>
+  );
 }
