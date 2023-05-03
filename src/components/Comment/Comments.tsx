99ab849587219d38d16e021b22de2f756a333fe7
Jinkyung Lee <83896975+ijinkyung@users.noreply.github.com>
2023-05-03 20:59:31 +0900

    Add : comment component 구현 완료

---
+++ src/components/Comment/Comments.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CommentList from './components/CommentList';
+import CommentInput from './components/CommentInput';
+
+export default function Comments() {
+  return (
+    <div>
+      <CommentList />
+      <CommentInput />
+    </div>
+  );
+}
