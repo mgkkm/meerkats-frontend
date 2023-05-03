acde4f6eb7a27ce4a75a28647f9adfffb99a5ff2
Jinkyung Lee <83896975+ijinkyung@users.noreply.github.com>
2023-05-03 19:00:07 +0900

    Add : header 기능 구현 완료

---
+++ src/components/Nav/components/SideBar.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const sideData = [
+  { id: 1, title: 'New' },
+  { id: 2, title: 'Korean Movies' },
+  { id: 3, title: 'International Movies' },
+  { id: 4, title: 'Korean  Series' },
+  { id: 5, title: 'International Series' },
+];
+
+export default function SideBar() {
+  const [show, setShow] = useState(false);
+  const side = useRef<HTMLDivElement>(null);
+
+  const showSideBar = () => {
+    setShow(show => !show);
+  };
+
+  const handleClose = (e: MouseEvent) => {
+    let sideArea = side.current;
+    let sideCildren = side.current?.contains(e.target as Node);
+    if (show && (!sideArea || !sideCildren)) {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', handleClose);
+    return () => {
+      window.removeEventListener('click', handleClose);
+    };
+  }, []);
+
+  return (
+    <>
+      <label
+        tabIndex={0}
+        onClick={showSideBar}
+        className="btn btn-ghost btn-circle"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-7 w-7"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16M4 18h7"
+          />
+        </svg>
+      </label>
+      <ul
+        tabIndex={0}
+        className={`menu dropdown-content mt-3 shadow bg-base-100  w-96 p-3.5 ${
+          show ? 'block' : 'hidden'
+        }`}
+      >
+        {sideData.map(list => {
+          return (
+            <li key={list.id} className="h-20 font-semibold ">
+              <a className="flex justify-between active:bg-mkOrange">
+                {list.title}
+                <span className="fa-solid fa-chevron-right text-mkOrange"></span>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
