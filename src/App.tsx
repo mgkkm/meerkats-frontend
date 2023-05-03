a361ba4df5a7c5fde32b0d86aee31cbdc65f0037
Jinkyung Lee <83896975+ijinkyung@users.noreply.github.com>
2023-05-03 19:25:48 +0900

    [Feat] header scroll 기능 구현 완료 (#1)
    feature/header
    
    - Add : header 기능 구현 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
+import ContentContainer from './components/ContentContainer/ContentContainer';
 
 export default function App() {
+  const [hide, setHide] = useState(false);
+
+  const scrollHandler = (e: React.WheelEvent<HTMLDivElement>) => {
+    if (e.deltaY < 0) {
+      setHide(true);
+    } else {
+      setHide(false);
+    }
+  };
   return (
     <>
-      <Nav />
-      <Outlet />
+      <Nav hide={hide} />
+      <ContentContainer scrollHandler={scrollHandler}>
+        <Outlet />
+      </ContentContainer>
       <Footer />
     </>
   );
---
+++ src/components/ContentContainer/ContentContainer.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react';
+// import TopBtn from '../TopBtn/TopBtn';
+
+type ScrollPorps = {
+  scrollHandler: (e: React.WheelEvent<HTMLDivElement>) => void;
+};
+
+export default function ContentContainer({
+  children,
+  scrollHandler,
+}: {
+  children: ReactNode;
+} & ScrollPorps) {
+  return (
+    <div className=" h-auto relative pb-28" onWheel={scrollHandler}>
+      {children}
+      {/* <TopBtn /> */}
+    </div>
+  );
+}
