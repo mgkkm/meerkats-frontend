e09d8755d2ec93466aab68ffbf2a3c479cb58a93
Jinkyung Lee <83896975+ijinkyung@users.noreply.github.com>
2023-05-07 15:23:13 +0900

    [Feat] 이벤트 -> 이벤트 디테일 페이지 이동 기능 구현 완료 / mock데이터로 통신 완료  (#7)
    feature/event
    
    - Add : 이벤트 디테일 페이지 이동 기능 구현 완료 / mock데이터로 통신 완료
    - Modify : 코드 리뷰 수정 완료

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,8 @@ import App from './App';
 import NotFound from './pages/NotFound/NotFound';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
+import EventList from './pages/Event/EventList';
+import EventDetail from './pages/Event/EventDetail';
 import KakaoLogin from './pages/Login/components/KakaoLogin';
 import BlogPosting from './pages/Blog/BlogPosting';
 import Membership from './pages/Membership/Membership';
@@ -16,6 +18,8 @@ export const router = createBrowserRouter([
     children: [
       { index: true, element: <Main /> },
       { path: '/login', element: <Login /> },
+      { path: '/event', element: <EventList /> },
+      { path: '/event/:id', element: <EventDetail /> },
       { path: '/kakaoRedirect', element: <KakaoLogin /> },
       { path: '/post', element: <BlogPosting /> },
       { path: '/membership', element: <Membership /> },
