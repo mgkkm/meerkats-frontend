161d94741ca11cfc0855361f458c3037fe295ba8
Jinkyung Lee <83896975+ijinkyung@users.noreply.github.com>
2023-05-07 16:45:40 +0900

    [Feat] 코드 리뷰 수정 - 상수데이터안에서 사용되던값 변수로  분리 완료 (#23)
    feature/main_
    
    - Modify : 영화 포스터 클릭시 영화상세로 이동 기능 구현 완료 / 각 포스터 별로 이동/hover 속성 적용 시키기 위해 컴포넌트 분리 완료
    - Modify : 코드 리뷰 수정 - 상수데이터안에서 사용되던값 변수로  분리 완료

---
+++ src/pages/Main/components/MainCarousel.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation, Autoplay } from 'swiper';
+import 'swiper/swiper.css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+export default function MainCarousel() {
+  return (
+    <Swiper
+      centeredSlides={true}
+      slidesPerView={1.5}
+      spaceBetween={50}
+      loop={true}
+      autoplay={{ delay: 5000 }}
+      navigation
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination, Navigation, Autoplay]}
+      className="h-[650px] mt-24"
+    >
+      <SwiperSlide>
+        <div className="h-full shadow-xl">
+          <img
+            className="m-auto w-full h-full "
+            alt="main_poster"
+            src="/images/Main/main_poster_1.jpeg"
+          />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="h-full shadow-xl">
+          <img
+            className="m-auto w-full h-full"
+            alt="main_poster"
+            src="/images/Main/main_poster_2.jpeg"
+          />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="h-full shadow-xl">
+          <img
+            className="m-auto w-full h-full"
+            alt="main_poster"
+            src="https://search.pstatic.net/common?quality=75&direct=true&src=https%3A%2F%2Fmovie-phinf.pstatic.net%2F20230420_107%2F1681956724322CsJcl_JPEG%2Fmovie_image.jpg"
+          />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="h-full shadow-xl">
+          <img
+            className="m-auto w-full h-full"
+            alt="main_poster"
+            src="https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2023/20230504_banner_143602.webp"
+          />
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
+}
