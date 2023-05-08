013922aca5c787c094a1100711fe3f4053875a21
hyun joo oh <58460131+hyunjoo1130@users.noreply.github.com>
2023-05-08 16:26:16 +0900

    [Refactor] 카카오 소셜 로그인 리팩토링 (#27)
    - Refactor: 데이터 타입 any 에서 구체적인 type으로 지정 / .env에 설정해준 BASE_URL 로 통신 기본 url 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,9 +4,9 @@ import App from './App';
 import NotFound from './pages/NotFound/NotFound';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
+import KakaoLogin from './pages/Login/components/KakaoLogin';
 import EventList from './pages/Event/EventList';
 import EventDetail from './pages/Event/EventDetail';
-import KakaoLogin from './pages/Login/components/KakaoLogin';
 import BlogPosting from './pages/Blog/BlogPosting';
 import Membership from './pages/Membership/Membership';
 import BlogDetail from './pages/Blog/BlogDetail/BlogDetail';
@@ -19,6 +19,7 @@ export const router = createBrowserRouter([
     children: [
       { index: true, element: <Main /> },
       { path: '/login', element: <Login /> },
+      { path: '/kakaoRedirect', element: <KakaoLogin /> },
       { path: '/event', element: <EventList /> },
       { path: '/event/:id', element: <EventDetail /> },
       { path: '/kakaoRedirect', element: <KakaoLogin /> },
