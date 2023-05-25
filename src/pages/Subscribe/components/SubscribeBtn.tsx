b4637ccb59dff64262a52a10d475386510e76bc7
Seoyun Kang <seoyunfleuve@gmail.com>
2023-05-26 02:26:49 +0900

    Add : PaymentInput 유효성 검사 기능 구현

--- src/pages/Subscribe/components/SubscribeBtn.tsx
+++ src/pages/Subscribe/components/SubscribeBtn.tsx
@@ -4,25 +4,28 @@ import { successAlert, warningAlert } from '../../../components/Alert/Modal';
 import useAxios from '../../../hooks/useAxios';
 import {
   paymentInputState,
+  paymentMethodState,
   subscriptionIdState,
+  validInputState,
 } from '../../../recoil/PaymentState';
 
 export default function SubscribeBtn() {
   const BASE_URL = process.env.REACT_APP_BASE_URL;
-
   const token = sessionStorage.getItem('token');
   const [loading, error, data, fetchData] = useAxios();
 
   const subscriptionId = useRecoilValue(subscriptionIdState);
+  const paymentMethod = useRecoilValue(paymentMethodState);
+  const paymentInput = useRecoilValue(paymentInputState);
 
-  const cardNumber = useRecoilValue(paymentInputState(1));
+  const { cardNumber, nameOnCard, expiryDate, dateOfBirth, email } =
+    paymentInput;
 
-  const expiryDate = useRecoilValue(paymentInputState(3));
-  const cardExpirationYear = expiryDate?.match(/.{1,2}/g)?.[1];
-  const cardExpirationMonth = expiryDate?.match(/.{1,2}/g)?.[0];
+  const cardExpirationYear = expiryDate.slice(2, 4);
+  const cardExpirationMonth = expiryDate.slice(0, 2);
 
-  const customerIdentityNumber = useRecoilValue(paymentInputState(4));
-  const customerEmail = useRecoilValue(paymentInputState(5));
+  const allValid = useRecoilValue(validInputState);
+  const isAllValid = Object.values(allValid).every(value => value === true);
 
   const paymentRequest = () => {
     fetchData({
@@ -33,11 +36,13 @@ export default function SubscribeBtn() {
       },
       data: {
         membershipId: subscriptionId,
+        paymentMethod: paymentMethod,
         cardNumber: cardNumber,
+        nameOnCard: nameOnCard,
         cardExpirationYear: cardExpirationYear,
         cardExpirationMonth: cardExpirationMonth,
-        customerIdentityNumber: customerIdentityNumber,
-        customerEmail: customerEmail,
+        customerIdentityNumber: dateOfBirth,
+        customerEmail: email,
       },
     }).then((result: any) => {
       if (result.message === 'SUCCESS') {
@@ -52,7 +57,7 @@ export default function SubscribeBtn() {
     <button
       className="btn bg-mkOrange border-none hover:bg-mkDarkOrange hover:border-none"
       onClick={() => paymentRequest()}
-      disabled={!token}
+      disabled={!isAllValid}
     >
       SUBSCRIBE
     </button>
