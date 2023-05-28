04c470f3ad40c8ce5d4ea79e32b5733d6cbfa159
Seoyun Kang <132238271+seoyunfleuve@users.noreply.github.com>
2023-05-28 14:24:55 +0900

    [Feat] PaymentInput 유효성 검사 기능 구현 (#70)
    feature/paymentInput
    
    - Add : PaymentInput 유효성 검사 기능 구현

--- src/pages/Subscribe/components/PaymentInfo.tsx
+++ src/pages/Subscribe/components/PaymentInfo.tsx
@@ -1,20 +1,32 @@
 import React, { useState } from 'react';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import {
+  paymentMethodState,
+  validInputState,
+} from '../../../recoil/PaymentState';
 import PaymentInput from './PaymentInput';
 
 export interface PaymentDetail {
   id: number;
+  name: string;
   detail: string;
   placeholder: string;
+  maxLength: number | undefined;
+  invalidMessage: string;
   width: number;
 }
 
 export default function PaymentInfo() {
   const [paymentBox, setPaymentBox] = useState(false);
+  const setPaymentMethod = useSetRecoilState(paymentMethodState);
+  const [allValid, setAllValid] = useRecoilState(validInputState);
   const [selectedPayment, setSelectedPayment] = useState('SELECT');
 
-  const paymentHandler = (payment: string) => {
+  const paymentHandler = (id: number, method: string) => {
     setPaymentBox(!paymentBox);
-    setSelectedPayment(payment);
+    setPaymentMethod(id);
+    setSelectedPayment(method);
+    setAllValid({ ...allValid, method: true });
   };
 
   return (
@@ -46,7 +58,7 @@ export default function PaymentInfo() {
                   <li
                     key={item.id}
                     className="text-sm cursor-pointer"
-                    onClick={() => paymentHandler(item.method)}
+                    onClick={() => paymentHandler(item.id, item.method)}
                   >
                     <a className="active:bg-mkLightGray rounded-md">
                       {item.method}
@@ -73,32 +85,47 @@ export default function PaymentInfo() {
 const PAYMENT_DETAILS = [
   {
     id: 1,
+    name: 'cardNumber',
     detail: 'Card Number',
     placeholder: '0000 0000 0000 0000',
+    maxLength: 19,
+    invalidMessage: '16자리 숫자를 입력해 주세요.',
     width: 52,
   },
   {
     id: 2,
+    name: 'nameOnCard',
     detail: 'Name on Card',
     placeholder: 'Thomas Anderson',
+    maxLength: undefined,
+    invalidMessage: '이름을 정확히 입력해 주세요.',
     width: 40,
   },
   {
     id: 3,
+    name: 'expiryDate',
     detail: 'Expiry Date',
     placeholder: 'MM/YY',
+    maxLength: 7,
+    invalidMessage: '날짜를 형식에 맞게 입력해 주세요.',
     width: 16,
   },
   {
     id: 4,
+    name: 'dateOfBirth',
     detail: 'Date of Birth',
     placeholder: 'YYMMDD',
+    maxLength: 6,
+    invalidMessage: '날짜를 형식에 맞게 입력해 주세요.',
     width: 24,
   },
   {
     id: 5,
+    name: 'email',
     detail: 'Email',
     placeholder: 'example@gmail.com',
+    maxLength: undefined,
+    invalidMessage: '이메일 주소를 형식에 맞게 입력해 주세요.',
     width: 52,
   },
 ];
--- src/recoil/PaymentState.tsx
+++ src/recoil/PaymentState.tsx
@@ -1,11 +1,36 @@
-import { atom, atomFamily } from 'recoil';
+import { atom } from 'recoil';
+import { PaymentInputData } from '../pages/Subscribe/components/PaymentInput';
 
 export const subscriptionIdState = atom({
   key: 'subscriptionIdState',
   default: 0,
 });
 
-export const paymentInputState = atomFamily<string, number>({
+export const paymentMethodState = atom<undefined | number>({
+  key: 'paymentMethodState',
+  default: undefined,
+});
+
+export const paymentInputState = atom<PaymentInputData>({
   key: 'paymentInputState',
-  default: '',
+  default: {
+    cardNumber: '',
+    nameOnCard: '',
+    expiryDate: '',
+    dateOfBirth: '',
+    email: '',
+  },
+});
+
+export const validInputState = atom({
+  key: 'validInputState',
+  default: {
+    method: false,
+    cardNumber: false,
+    nameOnCard: false,
+    expiryDate: false,
+    dateOfBirth: false,
+    email: false,
+    autoRenewAgreement: false,
+  },
 });
